9d53654719bf42da0647b1785f7cd11a07f8c83b
NastiaCandor <andubova98@gmail.com>
2023-05-07 19:04:34 +0300

    feat: html-builder done

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -6,15 +6,15 @@ fs.promises.readdir(secretDir, {withFileTypes: true})
   .then(filenames => {
     for (let filename of filenames) {
       const name = path.parse(filename.name).name;
-      fs.stat(path.join(secretDir, filename.name), (error, stats) => {
-        if (stats.isFile()) {
+      if (filename.isFile()) {
+        fs.stat(path.join(secretDir, filename.name), (error, stats) => {
           const ext = path.parse(filename.name).ext.slice(1);
           console.log(`${name} - ${ext} - ${stats.size}b`);
-        }
-      });
+        });
+      }
+      
     }
   })
-  // If promise is rejected
   .catch(err => {
     console.log(err);
   });
--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,38 +1,28 @@
 const fs = require('fs');
+const fsPromises = fs.promises;
 const path = require('path');
 
 const filesDir = path.join(__dirname, 'files');
 const filesCopyDir = path.join(__dirname, 'files-copy');
 
-function copyDir() {
-  fs.mkdir(filesCopyDir, { recursive: true }, error => {
-    if (error) console.log(error);
-  });
-  fs.promises.readdir(filesCopyDir)
-    .then(filenames => {
-      for (let filename of filenames) {
-        fs.unlink(path.join(filesCopyDir,filename), err => {
-          if (err) console.log(err);
-        });
-      }
-      fs.promises.readdir(filesDir, {withFileTypes: true})
-        .then(filenames => {
-          for (let filename of filenames) {
-            if (filename.isFile()) {
-              fs.copyFile(path.join(filesDir,filename.name),path.join(filesCopyDir,filename.name), err => {
-                if (err) console.log(err);
-              });
-            }
-          }
-        })
-        .catch(err => {
-          console.log(err);
-        });
-    })
-    .catch(err => {
-      console.log(err);
-    });
-  
+async function copyDir(toCopyDir, fromCopyDir) {
+  await fsPromises.mkdir(toCopyDir, { recursive:true });
+  await fsPromises.rm(toCopyDir, { recursive:true });
+  await fsPromises.mkdir(toCopyDir, { recursive:true });
+  const filenames = await fsPromises.readdir(fromCopyDir, {withFileTypes: true});
+  for (let filename of filenames) {
+    if (filename.isFile()) {
+      fs.copyFile(path.join(fromCopyDir,filename.name),path.join(toCopyDir,filename.name), err => {
+        if (err) console.log(err);
+      });
+    } else {
+      copyDir(path.join(toCopyDir, filename.name), path.join(fromCopyDir, filename.name));
+    }
+  }
 }
 
-copyDir();
+try {
+  copyDir(filesCopyDir, filesDir);
+} catch (err) {
+  console.log(err);
+}
